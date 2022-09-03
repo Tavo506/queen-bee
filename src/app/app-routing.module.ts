5371a6f434b67e6cbf430370adbed699b56034ae
Gustavo Blanco Alfaro <tavoba2015@gmail.com>
2022-09-03 14:48:59 -0600

    Visual fixes & routes data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { StudyComponent } from './pages/study/study.component';
 
 const routes: Routes = [
-  {path: "home", component: HomeComponent},
-  {path: "study", component: StudyComponent},
+  {path: "home", component: HomeComponent, data: {title: "Queen Bee - Spelling Bee Helper"}},
+  {path: "study", component: StudyComponent, data: {title: "Queen Bee - Spelling Bee Helper"}},
   {path: "**", redirectTo: "home"}
 ];
 
